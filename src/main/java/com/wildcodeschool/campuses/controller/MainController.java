28a65b5b499e6d4eb40c1f7287184f2f29e6a2a7
bastienwcs <bastien@wldcodeschool.fr>
2020-04-23 12:03:00 +0200

    solution

--- src/main/java/com/wildcodeschool/campuses/controller/MainController.java
+++ src/main/java/com/wildcodeschool/campuses/controller/MainController.java
@@ -12,9 +12,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    // TODO : /campuses route
+    @GetMapping("/campuses")
+    public String campusesRoutes(Model out){
+       
+        String firstName = "Anna";
+        out.addAttribute("firstName", firstName);
 
-    // TODO : say hello to the student, eg: "Hello Barbara"
+        Campus nantes = new Campus("Nantes", "c'est super!", "team/nantes");
+        Campus toulouse = new Campus("Toulouse", "vive la chocolatine", "team/toulouse");
+        Campus bordeaux = new Campus("Bordeaux", "j'ai rien à dire sur Bordeaux si il y a des bouchons et des bourgeois et des accents à la con", "team/loser");
 
-    // TODO : show a list of campuses
+        List<Campus> wild = new ArrayList<>();
+
+        wild.add(nantes);
+        wild.add(toulouse);
+        wild.add(bordeaux);
+
+        out.addAttribute("campusList", wild);
+        
+        return "campus";
+    }
+
 }
--- src/main/java/com/wildcodeschool/campuses/model/Campus.java
+++ src/main/java/com/wildcodeschool/campuses/model/Campus.java
@@ -2,6 +2,42 @@ package com.wildcodeschool.campuses.model;
 
 public class Campus {
 
-    // TODO
+    private String city;
+    private String description;
+    private String link;
+    
+    public Campus(String city, String description, String link) {
+
+        this.city = city;
+        this.description = description;
+        this.link = link;
+    }
+
+    public String getCity() {
+
+        return city;
+    }
+
+    public void setCity(String newCity) {
+
+        this.city = newCity;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLink() {
+        return this.link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
 
 }
