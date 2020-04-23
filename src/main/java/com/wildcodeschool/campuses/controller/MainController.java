29219112320536ae61489e4286dfd467d98714ef
bastienwcs <bastien@wldcodeschool.fr>
2020-04-23 10:48:09 +0200

    fix

--- src/main/java/com/wildcodeschool/campuses/controller/MainController.java
+++ src/main/java/com/wildcodeschool/campuses/controller/MainController.java
@@ -8,38 +8,13 @@ import com.wildcodeschool.campuses.model.Campus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-/*
-/campuses
-/team
-*/
 
 @Controller
 public class MainController{
 
+    // TODO : /campuses route
 
-    @GetMapping("/campuses")
-    public String campusController(Model model) {
+    // TODO : say hello to the student, eg: "Hello Barbara"
 
-        Campus nantes = new Campus("Nantes", "Super campus", "/team/nantes");
-        Campus biarritz = new Campus("Biarritz", "Moyen campus", "/team/biarritz");
-        Campus paris = new Campus("Paris", "Campus polué", "/team/paris");
-
-        List<Campus> listCampus = new ArrayList<>();
-        listCampus.add(nantes);
-        listCampus.add(biarritz);
-        listCampus.add(paris);
-        Boolean logged;
-        logged = false;
-        model.addAttribute("listCampus",listCampus);
-        model.addAttribute("logged", logged);
-
-        return "campus"; 
-    }
-    
-
-
-
-
+    // TODO : show a list of campuses
 }
