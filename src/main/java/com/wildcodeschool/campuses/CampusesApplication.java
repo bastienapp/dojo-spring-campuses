8cc49d2875f9b24cd8cf447bb5ffeca548e61181
bastienwcs <bastien@wldcodeschool.fr>
2020-04-23 08:58:19 +0200

    init

---
+++ src/main/java/com/wildcodeschool/campuses/CampusesApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.campuses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wildcodeschool/campuses/CampusesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.campuses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
